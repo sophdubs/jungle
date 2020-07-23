b5e72d20f69beebf71c783b5a4e47ca9366ec3c3
Sophie <sophiemargueritedubois@gmail.com>
2020-07-23 20:24:05 +0000

    Add status badge in admin sales table

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,14 @@
 class Sale < ActiveRecord::Base
+
+  def finished?
+    ends_on < Date.current 
+  end
+
+  def upcomming? 
+    starts_on > Date.current
+  end
+
+  def active? 
+    !upcomming? && !finished?
+  end
 end
